7d4af1e13e93da1277d7a5b2003b5c7626aeda40
imjihyeon00 <imjihyeon00@rocketstaff.com>
2025-03-02 11:24:40 +0900

    storybook test

--- src/stories/Skeleton.stories.js
+++ src/stories/Skeleton.stories.js
@@ -5,17 +5,24 @@ export default {
   title: "Components/Skeleton",
   component: Skeleton,
   argTypes: {
+    type: { control: "text" },
     width: { control: "text" },
     height: { control: "text" },
-    borderRadius: { control: "text" },
+    marginBottom: { control: "text" },
   },
 };
 
-const Template = (args) => <Skeleton {...args} />;
+const Template = (args) => (
+  <>
+    <Skeleton {...args} />
+    <Skeleton {...args} />
+  </>
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  width: "100px",
-  height: "20px",
-  borderRadius: "4px",
+  type: "text", // 'text', 'avatar'
+  width: "300px",
+  height: "18px",
+  marginBottom: "4px", // Text multiline
 };
